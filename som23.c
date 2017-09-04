54cfb4472a8181a1b8c89ddfba28383197a9cdd5
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2017-09-05 00:33:06 +0530

    Update som23.c

--- som23.c
+++ som23.c
@@ -1,24 +1,14 @@
-#include <stdio.h>  
-int getMinimum(int a,int b)
+#include<stdio.h>
+int main() 
 {
-if(a>=b)
-return b;
-else
-return a;
+int a,b,i,hcf;
+printf("enter two values\t");
+scanf("%d%d",&a,&b);
+for(i=1;i<= a||i<=b;i++) 
+{
+if(a%i==0 && b%i==0)
+hcf=i;
 }
-int main()
-{  
-int a,b,min,count,gcd=1;  
-printf("Enter two numbers\n");  
-scanf("%d %d",&a,&b);  
-min=getMinimum(a,b);
-for(count=1;count<=min;count++) 
-{  
-if(a%count==0 && b%count==0) 
-{
-gcd=count;  
-}  
-}  
-printf("GCD of %d and %d=%d\n",a,b,gcd);
-return 0;  
+printf("HCF=%d",hcf);
+return 0;
 }
